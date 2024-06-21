f61222b1eadfcdeb8a6c4774d357a276b41c6842
Isla Shi <46005300+islayshi@users.noreply.github.com>
2024-06-20 21:18:09 -0700

    Added react frontend

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,25 @@
 import express, { request } from "express";
 import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
-import { Book } from "./models/bookModel.js";
 import booksRoute from './routes/booksRoute.js';
+import cors from 'cors';
 
 const app = express();
 
 // Middleware for parsing request body
-app.use(express.json());
+//app.use(express.json());
+// Middle ware for handling CORS policy
+// Option 1: Allow all origins with default of cors(*)
+app.use(cors());
+
+// Option 2: allow custom origins (better way)
+app.use(
+    cors({
+        origin: 'http://localhost:3000',
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+        allowedHeaders: ['Content-Type'],
+    })
+);
 
 // Route for new saved book 
 app.get("/", (request, response) => {
